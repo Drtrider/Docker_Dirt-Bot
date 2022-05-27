30575a1693126868b99d5369cd52242e004af1c0
Jeret Shuck <drtrider200@gmail.com>
2022-05-27 15:15:41 -0700

    First commit

---
+++ Drt_bot.py
@@ -0,0 +1,12 @@
+# = = =   START File Setup   = = =
+import discord
+import os
+
+# ===============================
+# = = =   END File Setup   = = =
+# ===============================
+
+discordKey = os.environ['env_discord_api_key']
+
+print(discordKey)
+print("End of file...")
